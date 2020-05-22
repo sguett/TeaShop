436e9dd413b81a32e90f865493557061e27e38ce
sguett <guetta.samuel@gmail.com>
2020-05-22 16:16:56 +0200

    filters teas part1

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
-filterSelection("all") // Execute the function and show all columns
-function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("column");
-    if (c == "all") c = "";
-    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+var productsTea = {
+    products: ["product1", "product2", "product3", "product4"],
+    price: [10, 20, 30, 40],
+    category: ["classic", "Infusions", "Infusions", "Special Edition"],
+}
+
+// let products = document.getElementsByClassName("listCategoryItem2")[0];
+function filterByCat(arg) {
+    let categoryProducts = document.getElementsByClassName("listCategory")[0].children
+    let textCat = null;
+    console.log(arg)
+    if (arg == "item1") {
+        // textCat = categoryProducts[0].classList[0]
+        AllCards();
+    } else if (arg == "item2") {
+        textCat = categoryProducts[1].classList[0];
+        FilterCards(textCat);
+    } else if (arg == "item3") {
+        textCat = categoryProducts[2].classList[0]
+        FilterCards(textCat);
+    } else if (arg == "item4") {
+        textCat = categoryProducts[3].classList[0]
+        FilterCards(textCat);
     }
 }
 
-// Show filtered elements
-function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
+function AllCards() {
+    let cardContainer = document.getElementById("productsItems");
+    let cards = cardContainer.getElementsByClassName("myItems");
+    for (i = 0; i < cards.length; i++) {
+        cards[i].style.display = "";
+    }
+}
+
+function FilterCards(category) {
+    var input, filter, cards, cardContainer, h5, title, i;
+    input = document.getElementsByClassName(category)[0];
+    console.log(input)
+    input.setAttribute("style", "font-weight: bold");
+    filter = input.innerText.toUpperCase();
+    cardContainer = document.getElementById("productsItems");
+    cards = cardContainer.getElementsByClassName("myItems");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].firstElementChild.classList[0];
+        if (title.toUpperCase() == filter || filter.includes(title.toUpperCase())) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
         }
     }
 }
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
-    }
-    element.className = arr1.join(" ");
-}
+// update value in select price (filter)
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
+var rangeSlider = document.getElementById("rs-range-line");
+var rangeBullet = document.getElementById("rs-bullet");
+
+rangeSlider.addEventListener("input", showSliderValue, false);
+
+function showSliderValue() {
+    rangeBullet.innerHTML = rangeSlider.value;
+    var bulletPosition = (rangeSlider.value / rangeSlider.max);
+    rangeBullet.style.left = (bulletPosition * 578) + "px";
 }
