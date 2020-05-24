02c780005d7f75837828789a4bc2b1251b6b5444
sguett <guetta.samuel@gmail.com>
2020-05-24 15:32:28 +0200

    cart pay and add

--- script.js
+++ script.js
@@ -98,3 +98,73 @@ function showSliderValue() {
         } else { cards[i].style.display = "none"; }
     }
 }
+
+//// SHOPPING CART
+
+function addToCart(n) {
+    var tag = document.createElement("p");
+    var text = document.getElementsByTagName("button")[n].parentElement.children[1].innerText
+    var price = document.getElementsByTagName("button")[n].parentElement.children[2].innerText
+    var element = document.getElementById("cart");
+    var bool = false;
+    for (let i = 0; i < element.childElementCount; i++) {
+        let count = 1;
+        if (element.children[i].innerText.includes(text)) {
+            count = parseInt(element.children[i].innerText.substr(1, 1))
+            element.children[i].remove();
+            count++;
+            let FinalText = document.createTextNode("x" + count + " " + text + " " + price);
+            tag.appendChild(FinalText);
+            bool = true;
+        }
+    }
+    if (bool == false) {
+        let FinalText = document.createTextNode("x" + 1 + " " + text + " " + price);
+        tag.appendChild(FinalText);
+    }
+    element.appendChild(tag);
+}
+
+function clearCart() {
+    var element = document.getElementById("cart");
+    element.querySelectorAll('*').forEach(n => n.remove());
+}
+
+function payCart() {
+    var element = document.getElementById("cart");
+    var modal = document.getElementById("myModal");
+    var modalContent = document.getElementsByClassName("modal-content")[0].children[1];
+    var sum = 0;
+    if (element.childElementCount == 0) {
+        alert("You cart shopping is empty")
+        return;
+    } else {
+        // When the user clicks the button, open the modal 
+        modal.style.display = "block";
+        modalContent.innerText = "";
+        for (let i = 0; i < element.childElementCount; i++) {
+            sum = sum + (parseInt(element.children[i].innerText.split(" ")[3].substr(1, 2)) * parseInt(element.children[i].innerText.split(" ")[0].substr(1, 1)))
+        }
+        modalContent.innerText = element.innerText;
+        var tag = document.createElement("p");
+        var textSum = document.createTextNode("TOTAL AMOUNT DUE: " + sum)
+        tag.appendChild(textSum);
+        tag.style.fontWeight = "bold";
+        modalContent.appendChild(tag);
+        // element.querySelectorAll('*')
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+function close() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
